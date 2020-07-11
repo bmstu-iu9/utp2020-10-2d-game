c192d7035b5e497ce3af729daff190e322a8d4ec
Danya-Is <Armi.din1902@gmail.com>
2020-07-11 22:38:13 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const express = require('express')
-const app = express();
-var http = require('http').Server(app);
+const express = require('express'),
+   app = express(),
+   http = require('http').createServer(app),
+   io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
+let players = {}; //объект с парами типа id игрока : ник игрока
+
+//генерация псевдослучайного имени из символов ASCII от 32 до 126 включительно
+function getRandomName(){
+   let name = '';
+   for (let i = 0; i < 10; i++){
+      name += String.fromCharCode(Math.floor(Math.random() * 95) + 32);
+   }
+}
+
+io.on('connection', socket => {
+   players[socket.id] = getRandomName();
+   socket.on('disconnect', () => {
+      delete players[socket.id];
+   })
+})
+
+app.get('/', function (req, res) {
    res.sendfile('index.html');
 });
 app.use('/css', express.static(`${__dirname}/css`));
 
-http.listen(3000, function() {
+http.listen(3000, function () {
    console.log('listening on *:3000');
 });
