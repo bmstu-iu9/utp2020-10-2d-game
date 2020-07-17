18b2f9f7087c1da3f401a5a69394f44f64c32926
DbnStr <alexpich2002@yandex.ru>
2020-07-17 18:51:37 +0300

    can choose human and set the name

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const express = require('express'),
    app = express(),
    http = require('http').createServer(app),
    io = require('socket.io')(http);
-
+let zombies = []
+let humans = []
 io.on('connection', socket => {
    console.log('user connected');
+   socket.on('setHumanName' , function (name) {
+      if (humans.indexOf(name) == -1) {
+         humans.push(name)
+         console.log('a new player is ' + name)
+         socket.emit('PlayTheGame')
+      } else {
+         console.log(name)
+         socket.emit('usersExists', name + ' username is taken! Try some other username.');
+      }
+   })
    socket.on('disconnect', () => {
       console.log('user disconnected');
    })
