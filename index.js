b3a818cf5592e0df06f959ddd1d978eff0d2ba45
DbnStr <alexpich2002@yandex.ru>
2020-07-17 20:44:08 +0300

    can choose human and zombie. Nickname  is invalid

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const express = require('express'),
    app = express(),
    http = require('http').createServer(app),
    io = require('socket.io')(http);
-let zombies = []
-let humans = []
+let zombies = [];
+let humans = [];
 io.on('connection', socket => {
    console.log('user connected');
-   socket.on('setHumanName' , function (name) {
-      if (humans.indexOf(name) == -1) {
-         humans.push(name) //если ник не занят, то добавляем его в список "людей"
-         console.log('a new player is ' + name)
-         socket.emit('PlayTheGame')
-      } else
-         socket.emit('usersExists', name + ' username is taken! Try some other username.');
+   socket.on('setPlayerName' , function (player) {
+      if (humans.indexOf(player.name) == -1 && zombies.indexOf(player.name) == -1) {
+         if (player.name.length == 0)
+            socket.emit('usersExists', 'nickname is invalid');
+         else {
+            if (player.role.localeCompare("Human") == 0)
+               humans.push(player.name); //если ник не занят, то добавляем его в список "людей"
+            else zombies.push(player.name) //если ник не занят, то добавляем его в список "людей"
+            console.log('a new player ' + player.role + 'is ' + player.name);
+            socket.emit('PlayTheGame');
+         }
+      } else socket.emit('usersExists', player.name + ' username is taken! Try some other username.');
    })
    socket.on('disconnect', () => {
       console.log('user disconnected');
