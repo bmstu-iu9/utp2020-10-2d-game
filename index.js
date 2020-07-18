c5b649ce64514078781b663497d5df6dc40d91de
DbnStr <alexpich2002@yandex.ru>
2020-07-18 13:25:12 +0300

    can choose role. empty nickname is invalid.Added Map Players[socket.id][{name,role}]

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-
 'use strict'
-
 const express = require('express'),
    app = express(),
    http = require('http').createServer(app),
    io = require('socket.io')(http);
-let zombies = [];
-let humans = [];
+let players = new Map();
+function findName(name) {
+   for(let value of players)
+      if (value[1].name.localeCompare(name) == 0)
+         return 1;
+   return 0;
+}
 io.on('connection', socket => {
    console.log('user connected');
-   socket.on('setPlayerName' , function (player) {
-      if (humans.indexOf(player.name) == -1 && zombies.indexOf(player.name) == -1) {
-         if (player.name.length == 0)
-            socket.emit('usersExists', 'nickname is invalid');
-         else {
-            if (player.role.localeCompare("Human") == 0)
-               humans.push(player.name); //если ник не занят, то добавляем его в список "людей"
-            else zombies.push(player.name) //если ник не занят, то добавляем его в список "людей"
-            console.log('a new player ' + player.role + 'is ' + player.name);
+   socket.on('setPlayerName', function (player) {
+      if (player.name.length == 0) { //пустое имя недопустимо
+         socket.emit('invalidNickname', 'nickname is invalid');
+      } else {
+         if (findName(player.name) == 0) { //проверяем есть ли игок с таким ником
+            players.set(socket.id, {role: player.role, name: player.name});
+            console.log('a new player ' + player.name + ' is ' + player.role);
             socket.emit('PlayTheGame');
-         }
-      } else socket.emit('usersExists', player.name + ' username is taken! Try some other username.');
-   })
+         } else socket.emit('usersExists', player.name + ' username is taken! Try some other username.');
+      }
+   });
    socket.on('disconnect', () => {
-      console.log('user disconnected');
-   })
-})
+      if (players.get(socket.id)) {
+         console.log("Player " + players.get(socket.id).name + " disconnect");
+         players.delete(socket.id);
+      } else console.log("Player (no name) disconnect");
+   });
+});
 
 app.get('/', function (req, res) {
    res.sendfile('index.html');
