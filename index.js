f8b5369886ff9b48da1133506be7056cd4123f1e
Danya-Is <Armi.din1902@gmail.com>
2020-08-11 11:52:26 +0300

    simlify intersection with a line method

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ let players = {},
    pills = {},
    screenWidth,
    screenHeight,
-   outbreakRadius = 125,
    pillWidth = 50,
    pillHeight = 50,
    healthOfPill = 0.10;
@@ -56,6 +55,7 @@ class Rect {
    }
 }
 
+//класс для круговых объектов, используется как родительский
 class Circle {
    constructor(o, r) {
       this.o = o;
@@ -63,7 +63,18 @@ class Circle {
    }
    //проверка пересекает ли отрезок окружность this
    intersect(x1, y1, x2, y2) {
-      let a = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1),
+      //отрезок параллелен одной из сторон экрана
+      let s1 = findDist(new Point(x1, y1), new Point(this.o.x, this.o.y)),
+         s2 = findDist(new Point(x2, y2), new Point(this.o.x, this.o.y));
+      if (s1 <= this.radius || s2 <= this.radius)
+         return true;
+      else if ((x1 == x2 && x1 - this.o.x <= this.radius) ||
+         (y1 = y2 && y1 - this.o.x <= this.radius))
+         return true;
+      return false;
+
+      //общий случай для отрезков, непараллельным осям координат
+      /*let a = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1),
          b = 2 * ((x1 - this.o.x) * (x2 - x1) + (y1 - this.o.y) * (y2 - y1)),
          c = (x1 - this.o.x) * (x1 - this.o.x) +
             (y1 - this.o.y) * (y1 - this.o.y) -
@@ -82,7 +93,7 @@ class Circle {
             (x1 > x2 && curX <= x1 && curX >= x2))
             return true;
       }
-      return false;
+      return false;*/
    }
    //проверка находится ли точка внутри окружности this
    hasPoint(x, y) {
@@ -91,9 +102,10 @@ class Circle {
    }
 }
 
+//класс для вспышки эпидемии
 class Epidemic extends Circle {
-   constructor(o, r){
-      super (o, r);
+   constructor(o, r) {
+      super(o, r);
       this.marker = false; //отвечате за то, нужно ли отрисовывать облако эпидемии
       this.coordinateFixed = false; //зафисксированы ли координаты для будущего рисования
       this.start = Date.now();
